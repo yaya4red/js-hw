12aa3aea63b496ba22daf41807b99b30a3fa651a
yaya4red <yaya.hung@itrue.com.tw>
2013-10-11 15:55:51 +0800

    hw2與hw1預設顯示一個

--- js/hw1-pick-color.js
+++ js/hw1-pick-color.js
@@ -1,5 +1,6 @@
 var divhide = $(".thumbnail");
 		divhide.hide();
+$(".red").show();
 $("li").each(function() {
 		var li = $(this);
 		var getid = li.find("a").attr("href");
--- js/hw2.js
+++ js/hw2.js
@@ -3,8 +3,19 @@ $(".collapse.head").each(function() {
 	var getid = head.find("a").attr("href");
 	var push = $(getid);
 	push.hide();
-		head.on("click",function() {
-	  push.slideToggle("slow");
+	head.on("click",function() {
+  	var group = head.parents(".group");
+		if(group.hasClass("active")) {
+			group.removeClass("active");
+			push.slideUp("slow");
+		} else {
+			group.addClass("active");
+			push.slideDown("slow");
+		}
+  	group.siblings().each(function(){
+  		var other_group = $(this);
+  		other_group.find(".collapse.content").slideUp("slow");
+  	});
 	  return false;
 	});
 });
