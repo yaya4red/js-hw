2336cfa770cef371b1dace7d6e5a63057a1e9875
yaya4red <yaya.hung@itrue.com.tw>
2013-10-15 12:52:31 +0800

    hw1&hw2 卡關遇到同樣的問題了…

--- js/hw1-pick-color.js
+++ js/hw1-pick-color.js
@@ -1,13 +1,45 @@
-var divhide = $(".thumbnail");
-		divhide.hide();
-$(".red").show();
-$("li").each(function() {
-		var li = $(this);
-		var getid = li.find("a").attr("href");
-		var color = $(getid);
-		li.on("click",function(){
-			color.show();
-			color.siblings().hide();
-			return false;
-		});
+$(".container").each(function() {
+	var container = $(this);
+	var divhide = container.find(".thumbnail");
+	divhide.hide();
+	var red = container.find(".red");
+	red.show();
+	container.find(".row").each(function() {
+		var row = $(this);
+		row.find("ul").each(function() {
+			var ul = $(this);
+			ul.find("li").each(function() {
+				var li = $(this);
+				var getid = li.find("a").attr("href");
+				var color = $(getid);
+				li.on("click",function(){
+					color.show();
+					color.siblings().hide();
+					return false;
+				});
+			});
+		});	
+	});
 });
+
+
+
+// $(".container").each(function() {
+// 	var one = $(this);
+// 	var divhide = $(".thumbnail");
+// 			divhide.hide();
+// 	$(".red").each(function() {
+// 		var red = $(this);
+// 		red.show();
+// 	});
+// 	$("li").each(function() {
+// 			var li = $(this);
+// 			var getid = li.find("a").attr("href");
+// 			var color = $(getid);
+// 			li.on("click",function(){
+// 				color.show();
+// 				color.siblings().hide();
+// 				return false;
+// 			});
+// 	});
+// });
--- js/hw2.js
+++ js/hw2.js
@@ -1,24 +1,52 @@
-$(".collapse.head").each(function() {
-	var head = $(this);
-	var getid = head.find("a").attr("href");
-	var push = $(getid);
-	push.hide();
-	head.on("click",function() {
-  	var group = head.parents(".group");
-		if(group.hasClass("active")) {
-			group.removeClass("active");
-			push.slideUp("slow");
-		} else {
-			group.addClass("active");
-			push.slideDown("slow");
-		}
-  	group.siblings().each(function(){
-  		var other_group = $(this);
-  		other_group.find(".collapse.content").slideUp("slow");
-  	});
-	  return false;
+$(".momgroup").each(function() {
+	var mom = $(this);
+	mom.find(".collapse.head").each(function() {
+		var head = $(this);
+		head.find("a").each(function() {
+			var a = $(this);
+			var getid = a.attr("href")
+			var push = $(getid);
+			push.hide();
+			head.on("click",function() {
+		  	var group = head.parents(".group");
+				if(group.hasClass("active")) {
+					group.removeClass("active");
+					push.slideUp("slow");
+				} else {
+					group.addClass("active");
+					push.slideDown("slow");
+				}
+		  	group.siblings().each(function(){
+		  		var other_group = $(this);
+		  		other_group.find(".collapse.content").slideUp("slow");
+		  	});
+			  return false;
+			});	
+		});
 	});
 });
+
+// $(".collapse.head").each(function() {
+// 	var head = $(this);
+// 	var getid = head.find("a").attr("href");
+// 	var push = $(getid);
+// 	push.hide();
+// 	head.on("click",function() {
+//   	var group = head.parents(".group");
+// 		if(group.hasClass("active")) {
+// 			group.removeClass("active");
+// 			push.slideUp("slow");
+// 		} else {
+// 			group.addClass("active");
+// 			push.slideDown("slow");
+// 		}
+//   	group.siblings().each(function(){
+//   		var other_group = $(this);
+//   		other_group.find(".collapse.content").slideUp("slow");
+//   	});
+// 	  return false;
+// 	});
+// });
 
 $(".space.head").each(function() {
 	var space = $(this);
