07dc5969b038a189737b74dc663edc718fbce4c7
yaya4red <yaya.hung@itrue.com.tw>
2013-10-15 19:25:27 +0800

    hw3 追加子選單版本

--- js/hw3.js
+++ js/hw3.js
@@ -1,4 +1,5 @@
 $("ul").hide();
+$(".son-menu").hide();
 $(".group").each(function() {
 	var group = $(this);
 	group.find(".btn").each(function() {
@@ -17,11 +18,52 @@ $(".group").each(function() {
 		});
 		$(".item").find("a").each(function() {
 			var li = $(this);
-			li.on("click", function(){
+			li.mouseenter(function(){
+				li.siblings(".son-menu").each(function() {
+					var sonmenu = $(this);
+					sonmenu.show();
+				});
+				return false;
+			});
+			li.mouseleave(function(){
+				li.siblings(".son-menu").each(function() {
+					var sonmenu = $(this);
+					sonmenu.hide();
+				});
+				return false;
+			});
+			li.on("click", function() {
 				btn.removeClass("open");
 				ul.hide();
-				return false;
-			})
+			});
 		});
 	});
 });
+
+// $("ul").hide();
+// $(".group").each(function() {
+// 	var group = $(this);
+// 	group.find(".btn").each(function() {
+// 		var btn = $(this);
+// 		var ta = btn.parents(".group");
+// 		var ul = ta.find("ul");
+// 		btn.on("click", function() {
+// 		if (btn.hasClass("open")){
+// 			btn.removeClass("open");
+// 			ul.hide();
+// 		} else {
+// 			btn.addClass("open");
+// 			ul.show();
+// 		}
+// 		return false;
+// 		});
+// 		$(".item").find("a").each(function() {
+// 			var li = $(this);
+// 			li.on("click", function(){
+// 				btn.removeClass("open");
+// 				ul.hide();
+// 				return false;
+// 			})
+// 		});
+// 	});
+// });
