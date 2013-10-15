2f0b69718ddb6da877735877410bafa67040a585
yaya4red <yaya.hung@itrue.com.tw>
2013-10-15 16:17:28 +0800

    hw3 多個選單按鈕

--- js/hw3.js
+++ js/hw3.js
@@ -1,23 +1,27 @@
-var ul = $("ul.list-bg");
-ul.hide();
-$(".btn").each(function() {
-	var btn = $(this);
-	btn.on("click", function() {
-	if (btn.hasClass("open")){
-		btn.removeClass("open");
-		ul.hide();
-	} else {
-		btn.addClass("open");
-		ul.show();
-	}
-	return false;
-	});
-	$(".item").find("a").each(function() {
-		var li = $(this);
-		li.on("click", function(){
+$("ul").hide();
+$(".group").each(function() {
+	var group = $(this);
+	group.find(".btn").each(function() {
+		var btn = $(this);
+		var ta = btn.parents(".group");
+		var ul = ta.find("ul");
+		btn.on("click", function() {
+		if (btn.hasClass("open")){
 			btn.removeClass("open");
 			ul.hide();
-			return false;
-		})
+		} else {
+			btn.addClass("open");
+			ul.show();
+		}
+		return false;
+		});
+		$(".item").find("a").each(function() {
+			var li = $(this);
+			li.on("click", function(){
+				btn.removeClass("open");
+				ul.hide();
+				return false;
+			})
+		});
 	});
-})
+});
