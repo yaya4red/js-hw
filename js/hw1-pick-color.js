ae1e314835ffef4d785705a747da68cf4df7a78c
yaya4red <yaya.hung@itrue.com.tw>
2013-10-24 16:53:33 +0800

    hw1 調整兩個一樣的html互不影響

--- js/hw1-pick-color.js
+++ js/hw1-pick-color.js
@@ -11,7 +11,7 @@ $(".container").each(function() {
 			ul.find("li").each(function() {
 				var li = $(this);
 				var getid = li.find("a").attr("href");
-				var color = $(getid);
+				var color = $(container).find(getid);
 				li.on("click",function(){
 					color.show();
 					color.siblings().hide();
